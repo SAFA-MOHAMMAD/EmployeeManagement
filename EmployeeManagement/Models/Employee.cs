c5a043e843affdfeb61b830aeaff8cdba8060b9f
SAFA EMAD ISMAIL MOHAMMAD <Safa.mohad2003@gmail.com>
2022-11-26 21:24:59 +0300

    LAstest

--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -5,11 +5,15 @@ namespace EmployeeManagement.Models
     public class Employee
     {
         public int Id { get; set; }
-        [Required]
+        [Required (ErrorMessage ="The Name feiled is requird")]
+        [MaxLength(50,ErrorMessage ="The name should be lwss than 50 characters"),MinLength(4,ErrorMessage ="the minmum is 4 characters")]
         public String Name { get; set; }
+        [Display (Name="Ofice Email")]
+
         [Required]
-        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")]
-        public String Email { get; set; } 
-        public Dept Department { get; set; } 
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",ErrorMessage ="Invalid Email format")]
+        public String Email { get; set; }
+        [Required]
+        public Dept? Department { get; set; } 
     }
 }
